61043fcb6be7fcf38f300cb58bc0160a40512ea2
Akila Senevirathne <akilasenevirathne01@gmail.com>
2018-06-15 22:40:58 +0530

    Adding New History Page
    In here I added the new history page. When you click on "PLAYER HISTORY" button, you can redirect to the new history page.

---
+++ X-SMS/X-SMS/Controllers/HistoryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace X_SMS.Controllers
+{
+    public class HistoryController : Controller
+    {
+        /// <summary>
+        /// The History
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
