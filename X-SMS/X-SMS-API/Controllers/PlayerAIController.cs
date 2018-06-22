b21dfebc4cd8ed5045b9d3013a3588b7068debba
Imanshu Jayasinghe <imanshu15@gmail.com>
2018-06-22 12:46:37 +0530

    AI Controller

---
+++ X-SMS/X-SMS-API/Controllers/PlayerAIController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using X_SMS.AIHelper;
+using X_SMS_REP;
+
+namespace X_SMS_API.Controllers
+{
+    public class PlayerAIController : ApiController
+    {
+        [Route("api/PlayerAi/GetPlayerAIData")]
+        [HttpPost]
+        public ResultToken GetPlayerAIData([FromBody] GameDTO game)
+        {
+            ResultToken result = new ResultToken();
+            result.Success = true;
+
+            PlayerAI player = new PlayerAI(game);
+            result.Data = player.returnBuySellList();
+            return result;
+        }
+    }
+}
